a1a51d78a68b4bb1e4fc8da3ff185bc315219bef
zeldon-0 <48602270+zeldon-0@users.noreply.github.com>
2020-09-13 09:02:29 +0300

    Add trithemius encoding

---
+++ ZPD_1_1.Tests/TrithemiusTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using ZPD_1_1.Algorithms;
+using ZPD_1_1.Interfaces;
+using ZPD_1_1.Alphabets;
+namespace ZPD_1_1.Tests
+{
+    public class TrithemiusTest
+    {
+        [Theory]
+        [InlineData("ABCDZ", "ACEGD")]
+        [InlineData("BCVBVCBIJOBIJBOIFDBOIDJFOFDIJBDOIBDONBDOFNODINBFDNBOIFNXOIDFNBNDFOBIDFNBOIFDBOIJCVOIJBCVOBOCVOBIVJCBOICJJFGSJFODSNFODSNBVIDUBVCUBVCBVNDSONVSDNFASONASONDSOBFDOBHFDBFHDBFXCNBCXVBJFBKFNBC",
+            "BDXEZHHPRXLTVOCXVUTHCYFCMEDJLEHTOILXXMPBTCEUAGVAZKZNIGPASNJMVKXORBPXTWFUIDBAZNIKEYSNPIKEYMAPJDRZNCWWKFAIJGIVNKUKAWPZPFBQLZVNVQYRZUCCXQHXUUDBNYRNGDDRKHHYOLZEDPDKJIHMPMLQJPBQSONUDAXHDMBD")]
+        [InlineData("DGDBNBCXNSFOISNFOSDNF SDNFODSNF SDIFN SDJF NDSKJFN DSF DSH OUDIF ODBDJBF SUBFOS FA OAD PAND AIDOFN BSOFN 3 2N32OI N423N4 N``NPRNRPEWN SFSIFN SDONFSOINFSINFOISNFOSN FOISDN FOISDNF",
+            "DHFERGIEVBPZUFBUEJVGZ NZKDNDTPI WIOMV BNUR ARHAAXG XNB AQG OVFLJ TJILSLQ EHPUEJ XT IVZ MYMD BKGSKT IAXPY 3 2Z32BW C423D4 E``FILINMCVN THVMKT ZLXXQEBWCVJAGZJEPLEOTP ISNYKV OYTEQBU")]
+
+
+        public void Encode_MessageInEnglish_ReturnsMessageEncodedForEnglishAlphabet(string message, string expectedEncodedMessage)
+        {
+            TrithemiusCipher encoder = 
+                new TrithemiusCipher( new EnglishAlphabet());
+
+            string encodedMessage = encoder.Encode(message);
+
+            Assert.Equal(expectedEncodedMessage, encodedMessage);
+
+        }
+
+        [Theory]
+        [InlineData("Пароль", "ПБТСПВ")]
+        [InlineData("Ой на горі та й женці жнуть, А попід горою, яром - долиною козаки йдуть Гей, долиною, гей, широкою, козаки йдуть. Попереду Дорошенко Веде своє військо, Військо запорізьке хорошенько." +
+            " Гей, долиною, гей, широкою, Хорошенько. А позаду Сагайдачний, Що проміняв жінку на тютюн та люльку необачний. Гей, долиною, гей, широкою, Необачний.",
+            "ОК ПГ ЄУЦО ЬЗ У ПОЬЄЦ ФБЖЖН, С ЖЖИГЮ ЬЙМЛЩ, ЮРПО - ЖТРМФЦЄ ФЩСЙШХ ЩТЖЖН ФШГ, ЩИЖҐЇКШ, БДЙ, ЩЇУТПФД, ТЧПІЦУ ЧРЕЕЛ. ДДЄШИЬЬМ ЯЙМЛЦДНЛР ДИИЇ ШИЧН ЙУЦВЇЬВ, РЬАЗПҐЗ БЦЙЙМЖЄЩКЄ ЧСФУБЙХЕФЩ." +
+            " ЛПЧ, РБЯШГДП, ХЩҐ, РГКЙИЛЩ, ФОСРЮИТГСЦ. З ЩЩСЙПД ҐНСПАЦТНЖВЕ, УЙ ЛНМЛІОБД ЇМУСЮ ЦИ АИВЇЯ ДН АНВОГЙ ЖЮІШШУКЖЇ. ГЄЛ, ЖТРМФЦЄ, ЙНХ, ЖФВБЮГН, ҐЧЄФФПИҐЖ."
+)]
+
+        public void Encode_MessageInUkrainian_ReturnsMessageEncodedForUkrainianAlphabet(string message, string expectedEncodedMessage)
+        {
+            TrithemiusCipher encoder =
+                new TrithemiusCipher(new UkrainianAlphabet());
+
+            string encodedMessage = encoder.Encode(message);
+
+            Assert.Equal(expectedEncodedMessage, encodedMessage);
+
+        }
+
+
+    }
+}
---
+++ ZPD_1_1/Algorithms/TrithemiusCipher.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using ZPD_1_1.Interfaces;
+
+namespace ZPD_1_1.Algorithms
+{
+    public class TrithemiusCipher
+    {
+        private IAlphabet _alphabet;
+
+        public TrithemiusCipher(IAlphabet alphabet)
+        {
+            _alphabet = alphabet ?? throw new ArgumentNullException("Provided alphabet is null.");
+        }
+
+        public void SetAlphabet(IAlphabet alphabet)
+        {
+            _alphabet = alphabet ?? throw new ArgumentNullException("Provided alphabet is null.");
+        }
+
+        public string Encode(string message)
+        {
+
+            if (message == null)
+                throw new ArgumentNullException("Provided alphabet is null");
+
+            message = message.ToUpper();
+
+            StringBuilder encodedMessage = new StringBuilder();
+
+            Regex pattern = new Regex(@"\W|[0-9]");
+            int shift = 0;
+            foreach (char symbol in message)
+            {
+                if(pattern.Match(symbol.ToString()).Success)
+                {
+                    encodedMessage.Append(symbol);
+                }
+                else
+                {
+                    int originalPosition = _alphabet[symbol];
+                    char shiftedSymbol = _alphabet[(originalPosition + shift) % _alphabet.Size];
+                    shift++;
+
+                    encodedMessage.Append(shiftedSymbol);
+                }
+            }
+
+            return encodedMessage.ToString();
+        }
+    }
+}
